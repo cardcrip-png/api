68ec181ba119834e82362276d0e767b85701f780
cardcrip-png <cardcrip@gmail.com>
2026-01-02 20:59:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+export default function handler(req, res) {
+  const { nick } = req.query;
+
+  if (!nick) {
+    return res.status(400).json({
+      valido: false,
+      temporestante: 0
+    });
+  }
+
+  // 🔒 BASE SIMPLES DE USUÁRIOS (hardcoded)
+  const usuarios = {
+    pedro: {
+      valido: true,
+      temporestante: 180
+    },
+    teste: {
+      valido: false,
+      temporestante: 0
+    }
+  };
+
+  const usuario = usuarios[nick.toLowerCase()];
+
+  if (!usuario) {
+    return res.json({
+      nick,
+      valido: false,
+      temporestante: 0
+    });
+  }
+
+  return res.json({
+    nick,
+    valido: usuario.valido,
+    temporestante: usuario.temporestante
+  });
+}
